a6635fda336e20a208f83f5ae0fa166304bc8113
Eduardo Corona <eduardocorona@Eduardos-MacBook-Pro.local>
2022-02-08 12:47:08 -0800

    Order page update

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,14 +1,15 @@
+import { useState } from 'react';
+import Logo from '../../components/Logo/Logo';
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
 
-function AuthPage({ setUser }) {
+export default function AuthPage({ setUser }) {
+  const [showLogin, setShowLogin] = useState(true); 
   return (
     <main>
       <h1 className="auth-text">Sign up or Log in!</h1>
       <SignUpForm setUser={setUser} />
       <LoginForm setUser={setUser} />
     </main>
-  )
+  );
 }
-
-export default AuthPage;
